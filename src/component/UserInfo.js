bd2b0ca3682f12c6dd37f584b1aeafc172dcb893
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-21 00:04:29 +0530

    UI of the user info updated

--- src/component/UserInfo.js
+++ src/component/UserInfo.js
@@ -1,39 +1,56 @@
 import React from "react";
 import "./UserInfo.css";
 
-const UserInfo = () => (
+const UserInfo = ({ user }) => (
     <div className="user_info">
         <img
-            src="https://avatars.githubusercontent.com/u/5550850?v=4"
+            src={user.avatar_url}
             className="p-2 avatar"
             width="130"
             height="130"
             alt="profileimage"
         />
         <div className="user_infoDetails">
-            <h5>Username</h5>
+            <h5>{user.name}</h5>
             <div className="user_info_s">
-                <div className="user_infoDetail">
-                    <i className="fa fa-map-marker pr-1" />
-                    <span>Location</span>
-                </div>
-                <div className="user_infoDetail">
-                    <i className="fa fa-globe pr-1" />
-                    <span>BlogURL</span>
-                </div>
-                <div className="user_infoDetail">
-                    <i className="fa fa-twitter pr-1" />
-                    <span>Twitter Userid</span>
-                </div>
-                <div className="user_infoDetail">
-                    <i className="fa fa-envelope pr-1" />
-                    <span>Email</span>
-                </div>
-                <div className="user_infoDetail">
-                    <i className="fa fa-envelope pr-1" />
-                    <span>Bio</span>
-                </div>
+                {user.bio && (
+                    <div className="user_infoDetailBio">
+                        <span>{user.bio}</span>
+                    </div>
+                )}
+            </div>
+            <div className="user_info_s">
+                {user.location && (
+                    <div className="user_infoDetail">
+                        <i className="fa fa-map-marker pr-1" />
+                        <span style={{marginLeft : "5px"}}>{user.location} ·</span>
 
+                    </div>
+                )}
+                {user.blog && (
+                    <div className="user_infoDetail">
+                        <i className="fa fa-globe pr-1" />
+                        <span style={{marginLeft : "5px"}}>{user.blog} ·</span>
+                    </div>
+                )}
+                {user.twitter_username && (
+                    <div className="user_infoDetail">
+                        <i className="fa fa-twitter pr-1" />
+                        <span style={{marginLeft : "5px"}}>{user.twitter_username} ·</span>
+                    </div>
+                )}
+                {user.email && (
+                    <div className="user_infoDetail">
+                        <i className="fa fa-envelope pr-1" />
+                        <span style={{marginLeft : "5px"}}>{user.email} ·</span>
+                    </div>
+                )}
+                {user.html_url && (
+                    <div className="user_infoDetail">
+                        <i className="fa fa-github pr-1" />
+                        <span style={{marginLeft : "5px"}}>{user.html_url}</span>
+                    </div>
+                )}
             </div>
         </div>
     </div>
