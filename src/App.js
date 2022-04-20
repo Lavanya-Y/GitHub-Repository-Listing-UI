ca0989c6bf88a23986ec8170ebce5ced3773545b
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-20 22:57:40 +0530

    Changes made with api files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import UserPage from "./container/UserPage";
-
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 
 function App() {
   return (
-      <UserPage/>
+      <BrowserRouter>
+        <Switch>
+          <Route path = "/:userName" component={UserPage} />
+          <Redirect to = {'/bradtraversy'} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
