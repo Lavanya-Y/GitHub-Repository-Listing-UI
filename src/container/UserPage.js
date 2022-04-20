2c933f6992985f5fb9d175d4fa4a1ff18e648894
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-20 22:07:50 +0530

    Userinfo edited in the UI page

--- src/container/UserPage.js
+++ src/container/UserPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserInfo from "../component/UserInfo";
 import Navbar from "../component/Navbar";
+import Repositories from "../component/Repositories";
 
 function UserPage() {
     return (
         <div className="app">
             <Navbar/>
             <UserInfo/>
+            <Repositories/>
         </div>
     )
 }
