5bc9f4d9b159fb85c709d7c34c3e067dc9db092f
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-21 19:31:10 +0530

    Each repository UI changed

--- src/component/Repository.js
+++ src/component/Repository.js
@@ -2,25 +2,29 @@ import React from "react";
 
 import "./Repositories.css";
 
-const Repository = () => (
+const Repository = ({user, repo}) => (
     <div className="repo">
         <div>
             <div className="title">
                 <i className="fa fa-book pr-1" />
-                <a href="#">
-                    <span>Repo Name</span>
+                <a target="_blank" rel="noreferrer_noopener" href={repo.html_url}>
+                    <span style={{marginLeft : "5px"}}>{repo.name}</span>
                 </a>
             </div>
-            <span className="desc">Description</span>
+            <span className="desc">{repo.description}</span>
         </div>
         <div className="d-flex">
             <div className="subitem">
-                <i className="fa fa-code pr-1" />
-                <span>Language</span>
+                {repo.language && (
+                    <div>
+                        <i className="fa fa-code pr-1" />
+                        <span style={{marginLeft : "2.5px"}}>{repo.language}</span>
+                    </div>
+                )}
             </div>
             <div className="subitem">
                 <i className="fa fa-star pr-1" />
-                <span>Star</span>
+                <span style={{marginLeft : "2.5px"}}>{repo.stargazers_count}</span>
             </div>
         </div>
     </div>
