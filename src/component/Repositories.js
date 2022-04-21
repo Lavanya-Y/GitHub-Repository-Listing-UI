95f1ff32395f66c42c1c2da43818049a000730a4
Lavanya-Y <lavanyamiliy@gmail.com>
2022-04-21 19:30:51 +0530

    Showing set of repositories.

--- src/component/Repositories.js
+++ src/component/Repositories.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Repository from "./Repository";
+import "./Repository.css";
 
-const Repositories = () => (
-    <Repository/>
+const Repositories = ({user, repos}) => (
+    <section className="repositories">
+        {
+            repos.map((repo) => (
+                <Repository key={repo.id} repo={repo} user={user} />
+            ))
+        }
+    </section>
 )
 
 export default Repositories
